8cf18acccd41c737e26729f87f55044596133497
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-13 08:53:46 +0530

    build middleware workflow

---
+++ sqlquery/middleware.py
@@ -0,0 +1,14 @@
+def new_middleware(get_response):
+
+
+    def middleware(request):
+
+        print("before")
+
+        response = get_response(request)
+
+        print("after")
+
+        return response
+
+    return middleware
