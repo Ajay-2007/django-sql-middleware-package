306ca50cd54c732c414b38c14eb53af7a5289011
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-13 08:12:17 +0530

    capturing sql queries in the terminal

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,10 +3,18 @@ from .models import Product
 import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
+from django.db import connection
 
 def home(request):
     qs = Product.objects.all()
-    serialized_data = serialize("json", qs)
+
+
+
+
+    serialized_data = serialize("json", qs) # when we call qs then only django performs a query to the database
+
+    print(connection.queries)
+
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)
 
