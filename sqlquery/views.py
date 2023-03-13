f17fb6b00267d642be3f0740f7f49d168f79fda3
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-13 08:34:05 +0530

    pretty print sql queries

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -4,16 +4,21 @@ import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.db import connection
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
+from sqlparse import format
 
 def home(request):
     qs = Product.objects.all()
 
-
-
-
     serialized_data = serialize("json", qs) # when we call qs then only django performs a query to the database
 
-    print(connection.queries)
+    q = list(connection.queries)
+
+    for qs in q:
+        sqlformatted = format(str(qs["sql"]), reindent=True)
+        print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
 
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)
