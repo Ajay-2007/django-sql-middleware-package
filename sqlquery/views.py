74999942269c0e8f7d8eb2b8671702e82755cc85
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-13 07:51:21 +0530

    create a django url

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -8,5 +8,5 @@ def home(request):
     qs = Product.objects.all()
     serialized_data = serialize("json", qs)
     serialized_data = json.loads(serialized_data)
-    return JsonResponse(serialized_data, status=200)
+    return JsonResponse(serialized_data, safe=False, status=200)
 
